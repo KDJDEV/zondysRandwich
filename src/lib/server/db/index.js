983f7f5ce38ab89134aa36d80029bcd36d2beee1
KDJDEV <kyledagman7@gmail.com>
2025-04-09 17:15:43 -0400

    switched to postgres with supabase

--- src/lib/server/db/index.js
+++ src/lib/server/db/index.js
@@ -1,3 +1,5 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from './schema';
--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -1,6 +1,17 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { pgTable, serial, text, varchar, integer, foreignKey } from "drizzle-orm/pg-core";
 
-export const user = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	age: integer('age')
+export const users = pgTable('users', {
+	id: serial('id').primaryKey(),
+	fullName: text('full_name'),
+	email: varchar('email', { length: 256 }).unique(),
+	password: varchar('password', { length: 256 }),
+});
+
+export const sandwiches = pgTable('sandwiches', {
+	id: serial('id').primaryKey(),
+	bread: text('bread'),
+	protein: text('protein'),
+	cheese: text('cheese'),
+	toppings: text('toppings').array(),
+	userId: integer('user_id').notNull().references(() => users.id),
 });
