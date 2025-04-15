72dbc4506391f978d38190f5c27cb87d20fe787c
KDJDEV <kyledagman7@gmail.com>
2025-04-15 00:11:12 -0400

    allow supabase images

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
 import { handler } from './build/handler.js';
 import express from 'express';
 import helmet from "helmet";
 import http from 'http';
-import { createTerminus } from '@godaddy/terminus'
+import { createTerminus } from '@godaddy/terminus';
 
 const app = express();
 
@@ -14,17 +14,18 @@ app.use(
       directives: {
         ...helmet.contentSecurityPolicy.getDefaultDirectives(),
         "script-src": ["'self'", "'unsafe-inline'"],
+        "img-src": ["'self'", "https://cqthaatacrjtizijviwf.supabase.co"] // Add your external image source here
       }
     },
     referrerPolicy: {
       policy: ["same-origin"],
     },
   })
-)
+);
 
 app.use(handler);
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 createTerminus(server, {
   signals: ['SIGTERM', 'SIGINT'],
@@ -32,7 +33,7 @@ createTerminus(server, {
     // Call your cleanup functions below. For example:
     // db.shutdown()
   }
-})
+});
 
 server.listen(3000, () => {
   console.log('Listening on port 3000');
